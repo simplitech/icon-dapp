36b96e25ab0bad812ba3a0fc95dcd069591791dc
luc10921 <lucas.uezu@simpli.com.br>
2023-09-11 17:15:17 -0300

    CU-86a0pp97d - Add a new method that returns all icons in the smart contract

--- sdk/src/types/index.ts
+++ sdk/src/types/index.ts
@@ -8,6 +8,11 @@ export type SmartContractConfig = {
 }
 
 export type IconProperties = {
-  icon288x288: string | null
-  icon25x25: string | null
+  "icon/288x288": string | null
+  "icon/25x25": string | null
+}
+
+export type ScriptHashAndIcons = {
+  scriptHash: string,
+  icons: IconProperties & Record<string, any>  // Another property could be added to the smart contract
 }
